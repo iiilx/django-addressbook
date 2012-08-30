d580d9bad62ecf33be7828abddcaadf53bff29c9
Yugal Jindle <yugal.jindle@joshlabs.in>
2012-08-30 16:45:34 +0530

    Added Website, SocialNetworks to models with Privacy settings

--- addressbook/models.py
+++ addressbook/models.py
@@ -3,30 +3,42 @@ from django.contrib.localflavor.us.models import USStateField, PhoneNumberField
 from django.contrib.auth.models import User
 
 ADR_TYPES = (
-#    ('INTL', 'INTL'),
-#    ('POSTAL', 'POSTAL'),
-#    ('PARCEL', 'PARCEL'),
-    ('WORK', 'WORK'),
-#    ('DOM', 'DOM'),
-    ('HOME', 'HOME'),
-#    ('PREF', 'PREF'),
+    ('Home', 'Home'),
+    ('Work', 'Work'),
 )
 
 TEL_TYPES = (
-    ('HOME', 'HOME'),
-#    ('MSG', 'MSG'),
-    ('WORK', 'WORK'),
-#    ('PREF', 'PREF'),
-    ('FAX', 'FAX'),
-    ('CELL', 'CELL'),
+    ('Mobile', 'Mobile'),
+    ('Home', 'Home'),
+    ('Office', 'Office'),
+    ('Fax', 'Fax'),
 )
 
 EMAIL_TYPES = (
-    ('HOME', 'HOME'),
-    ('WORK', 'WORK'),
-#    ('INTERNET', 'INTERNET'),
-#    ('PREF', 'PREF'),
+    ('Home', 'Home'),
+    ('Work', 'Work'),
 )
+
+PRIVACY_TYPES = (
+    ('Public', 'Public'),
+    ('Private', 'Private'),
+)
+
+WEBSITE_TYPES = (
+    ('Work', 'Work'),
+    ('Personal', 'Personal'),
+    ('Portfolio', 'Portfolio'),
+    ('Blog', 'Blog'),
+)
+
+SOCNET_TYPES = (
+    ('Skype', 'Skype'),
+    ('LinkedIn', 'LinkedIn'),
+    ('Facebook', 'Facebook'),
+    ('Instagram', 'Instagram'),
+    ('Pinterest', 'Pinterest'),
+)
+
 
 class ContactGroup(models.Model):
     user = models.ForeignKey(User)
@@ -57,6 +69,7 @@ class Address(models.Model):
     state = USStateField()
     zip = models.CharField(max_length = "10") 
     type = models.CharField(max_length="5", choices = ADR_TYPES)
+    privacy = models.CharField(max_length="10", choices = PRIVACY_TYPES)
 
     def __unicode__(self):
         return '%s %s: %s %s, %s' % (self.contact.first_name, self.contact.last_name, self.street, self.city, self.state)
@@ -65,6 +78,7 @@ class PhoneNumber(models.Model):
     contact = models.ForeignKey(Contact)
     phone = PhoneNumberField()
     type = models.CharField(max_length="4", choices = TEL_TYPES)
+    privacy = models.CharField(max_length="10", choices = PRIVACY_TYPES)
 
     def __unicode__(self):
         return "%s %s: %s" % (self.contact.first_name, self.contact.last_name, self.phone)
@@ -73,8 +87,26 @@ class Email(models.Model):
     contact = models.ForeignKey(Contact)
     email = models.EmailField() 
     type = models.CharField(max_length="8", choices = EMAIL_TYPES) 
-    
+    privacy = models.CharField(max_length="10", choices = PRIVACY_TYPES)
+
     def __unicode__(self):
         return "%s %s: %s" % (self.contact.first_name, self.contact.last_name, self.email)
 
+class Website(models.Model):
+    contact = models.ForeignKey(Contact)
+    website = models.URLField(verify_exists = False, blank = True)
+    type = models.CharField(max_length="20", choices = WEBSITE_TYPES) 
+    privacy = models.CharField(max_length="10", choices = PRIVACY_TYPES)
+
+    def __unicode__(self):
+        return "%s %s: %s" % (self.contact.first_name, self.type, self.website)
+
+class SocialNetworks(models.Model):
+    contact = models.ForeignKey(Contact)
+    handle = models.CharField(max_length = "50")
+    type = models.CharField(max_length="20", choices = SOCNET_TYPES)
+    privacy = models.CharField(max_length="10", choices = PRIVACY_TYPES)
+
+    def __unicode__(self):
+        return "%s %s: %s" % (self.contact.first_name, self.type, self.website)
 
